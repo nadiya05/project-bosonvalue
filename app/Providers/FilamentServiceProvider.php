272847ee925b51c7e87841afd077a217a779659c
nadiya05 <nadiyazarahislamiati@Nadiyas-MacBook-Pro.local>
2026-02-12 13:52:56 +0700

    downgrade filament version

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    public function register() {}
+    public function boot() {}
+}
